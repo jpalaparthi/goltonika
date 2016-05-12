450ddfc1f356b76dd8512821e412c47575408544
jpalaparthi <jiten.palaparthi@gmail.com>
2016-05-12 19:48:39 +0530

    getIP routine has been added

--- src/tcp/main.go
+++ src/tcp/main.go
@@ -2,44 +2,49 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"server"
 	"fileops"
-	"time"
+	"fmt"
 	"log"
+	"os"
+	"os/exec"
+	"server"
+	"time"
 )
 
 var logger *log.Logger
 
 func main() {
 	var addr string
-	
-	if len(os.Args) > 1 {
+
+	/*if len(os.Args) > 1 {
 		addr = os.Args[1]
 	} else {
 		fmt.Println("IP address is not provided.\nPlease provide IP address and port(e.g. 10.12.43.345:6000) as commong line argument")
 		return
+	}*/
+	addr, err := getIP()
+	if err != nil {
+		panic(err)
 	}
-	
+
 	s := &server.Server{}
 
 	go OnMessage(s)
-	
-   	s.Init(addr)
-	
+
+	s.Init(addr)
+
 	s.Listen()
-	
-    go s.AddOrRemoveClient(getConLogger("ActiveClients.txt"))
-	
+
+	go s.AddOrRemoveClient(getConLogger("ActiveClients.txt"))
+
 	//Just uncomment the below code to check connection object incoming data
 	//go ShowConnections(s)
-	
+
 	s.ListenAndAccept()
 
 }
 
-func  OnMessage(s *server.Server) {
+func OnMessage(s *server.Server) {
 	for m := range s.Message {
 		fmt.Println(m.MSG)
 		switch m.MSGType {
@@ -73,25 +78,34 @@ func logMessage(logger *log.Logger, filename, subject, Message string) {
 	logfile, _ := os.OpenFile(filename, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	logger = log.New(logfile, subject, log.Lshortfile|log.LstdFlags)
 	logger.Println(Message)
-	}
+}
 
-func getConLogger(filename string)(*log.Logger){
+func getConLogger(filename string) *log.Logger {
 	var conlogger *log.Logger
 	file, _ := os.OpenFile(filename, os.O_RDWR|os.O_CREATE, 0666)
 	file.Truncate(0) // this deletes the contents of the file every time and recreates the active clients data
 	conlogger = log.New(file, "Acive Connecions: ", log.Lshortfile|log.LstdFlags)
-    return conlogger
-	}
+	return conlogger
+}
 
 func ShowConnections(s *server.Server) {
-	for{
-		time.Sleep(time.Second*10)
-	fmt.Println("Displaying the List of the connected clients")
-	for i := 0; i < len(s.Clients); i++ {
-		fmt.Println(s.Clients[i].Conn.RemoteAddr().String())
-		fmt.Println(s.Clients[i].InData)
-		fmt.Println()
-		
+	for {
+		time.Sleep(time.Second * 10)
+		fmt.Println("Displaying the List of the connected clients")
+		for i := 0; i < len(s.Clients); i++ {
+			fmt.Println(s.Clients[i].Conn.RemoteAddr().String())
+			fmt.Println(s.Clients[i].InData)
+			fmt.Println()
+
 		}
 	}
+}
+
+func getIP() (string, error) {
+	cmd := "ip route get 8.8.8.8 | awk '{print $NF; exit}'"
+	out, err := exec.Command("bash", "-c", cmd).Output()
+	if err != nil {
+		return "", err
+	}
+	return string(out), err
 }
